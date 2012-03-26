4e92ecc9d9b921da5a1865d46beb0811b7587c6c
David Mitchel <git@mitzip.com>
2012-03-25 23:47:11 -0500

    Last half of formatting changes

--- MongoSession.php
+++ MongoSession.php
@@ -58,48 +58,48 @@ class MongoSession {
 	 * @param   array   $config
 	 */
 	public function __construct($config = array()) {
-	// initialize the database
-	$this->_init(empty($config) ? $this->_config : $config);
+		// initialize the database
+		$this->_init(empty($config) ? $this->_config : $config);
 
-	// set object as the save handler
-	session_set_save_handler(
-		array(&$this, 'open'),
-		array(&$this, 'close'),
-		array(&$this, 'read'),
-		array(&$this, 'write'),
-		array(&$this, 'destroy'),
-		array(&$this, 'gc')
-	);
+		// set object as the save handler
+		session_set_save_handler(
+			array(&$this, 'open'),
+			array(&$this, 'close'),
+			array(&$this, 'read'),
+			array(&$this, 'write'),
+			array(&$this, 'destroy'),
+			array(&$this, 'gc')
+		);
 
-	// set some important session vars
-	ini_set('session.auto_start',               0);
-	ini_set('session.gc_probability',           1);
-	ini_set('session.gc_divisor',               100);
-	ini_set('session.gc_maxlifetime',           $this->_config['lifetime']);
-	ini_set('session.referer_check',            '');
-	ini_set('session.entropy_file',             '/dev/urandom');
-	ini_set('session.entropy_length',           16);
-	ini_set('session.use_cookies',              1);
-	ini_set('session.use_only_cookies',         1);
-	ini_set('session.use_trans_sid',            0);
-	ini_set('session.hash_function',            1);
-	ini_set('session.hash_bits_per_character',  5);
+		// set some important session vars
+		ini_set('session.auto_start',               0);
+		ini_set('session.gc_probability',           1);
+		ini_set('session.gc_divisor',               100);
+		ini_set('session.gc_maxlifetime',           $this->_config['lifetime']);
+		ini_set('session.referer_check',            '');
+		ini_set('session.entropy_file',             '/dev/urandom');
+		ini_set('session.entropy_length',           16);
+		ini_set('session.use_cookies',              1);
+		ini_set('session.use_only_cookies',         1);
+		ini_set('session.use_trans_sid',            0);
+		ini_set('session.hash_function',            1);
+		ini_set('session.hash_bits_per_character',  5);
 
-	// disable client/proxy caching
-	session_cache_limiter('nocache');
+		// disable client/proxy caching
+		session_cache_limiter('nocache');
 
-	// set the cookie parameters
-	session_set_cookie_params(
-		$this->_config['lifetime'],
-		$this->_config['cookie_path'],
-		$this->_config['cookie_domain']
-	);
+		// set the cookie parameters
+		session_set_cookie_params(
+			$this->_config['lifetime'],
+			$this->_config['cookie_path'],
+			$this->_config['cookie_domain']
+		);
 
-	// name the session
-	session_name('mongo_sess');
+		// name the session
+		session_name('mongo_sess');
 
-	// start it up
-	session_start();
+		// start it up
+		session_start();
 	}
 
 	/**
@@ -110,33 +110,33 @@ class MongoSession {
 	 * @param   array   $config
 	 */
 	private function _init($config) {
-	// ensure they supplied a database
-	if (empty($config['database'])) {
-		throw new Exception('You must specify a MongoDB database to use for session storage.');
-	}
-
-	if (empty($config['collection'])) {
-		throw new Exception('You must specify a MongoDB collection to use for session storage.');
-	}
-
-	// update config
-	$this->_config = $config;
-
-	// generate server connection strings
-	$connections = array();
-	if (!empty($this->_config['servers'])) {
-		foreach ($this->_config['servers'] as $server) {
-			$str = '';
-			if (!empty($server['username']) && !empty($server['password'])) {
-				$str .= $server['username'] . ':' . $server['password'] . '@';
-			}
-			$str .= !empty($server['host']) ? $server['host'] : Mongo::DEFAULT_HOST;
-			$str .= ':' . (!empty($server['port']) ? (int) $server['port'] : Mongo::DEFAULT_PORT);
-			array_push($connections, $str);
+		// ensure they supplied a database
+		if (empty($config['database'])) {
+			throw new Exception('You must specify a MongoDB database to use for session storage.');
 		}
+
+		if (empty($config['collection'])) {
+			throw new Exception('You must specify a MongoDB collection to use for session storage.');
+		}
+
+		// update config
+		$this->_config = $config;
+
+		// generate server connection strings
+		$connections = array();
+		if (!empty($this->_config['servers'])) {
+			foreach ($this->_config['servers'] as $server) {
+				$str = '';
+				if (!empty($server['username']) && !empty($server['password'])) {
+					$str .= $server['username'] . ':' . $server['password'] . '@';
+				}
+				$str .= !empty($server['host']) ? $server['host'] : Mongo::DEFAULT_HOST;
+				$str .= ':' . (!empty($server['port']) ? (int) $server['port'] : Mongo::DEFAULT_PORT);
+				array_push($connections, $str);
+			}
 		} else {
-		// use default connection settings
-		array_push($connections, Mongo::DEFAULT_HOST . ':' . Mongo::DEFAULT_PORT);
+			// use default connection settings
+			array_push($connections, Mongo::DEFAULT_HOST . ':' . Mongo::DEFAULT_PORT);
 		}
 
 		// add immediate connection, although this is the default driver action
@@ -189,41 +189,41 @@ class MongoSession {
 		);
 	}
 
-    /**
-     * Open does absolutely nothing as we already have an open connection.
-     *
-     * @access  public
-     * @return	bool
-     */
-    public function open($save_path, $session_name) {
-        return true;
-    }
+	/**
+	 * Open does absolutely nothing as we already have an open connection.
+	 *
+	 * @access  public
+	 * @return	bool
+	 */
+	public function open($save_path, $session_name) {
+		return true;
+	}
 
-    /**
-     * Close does absolutely nothing as we can assume __destruct handles
-     * things just fine.
-     *
-     * @access  public
-     * @return	bool
-     */
-    public function close() {
-        return true;
-    }
+	/**
+	 * Close does absolutely nothing as we can assume __destruct handles
+	 * things just fine.
+	 *
+	 * @access  public
+	 * @return	bool
+	 */
+	public function close() {
+		return true;
+	}
 
-    /**
-     * Read the session data.
-     *
-     * @access	public
-     * @param	string	$id
-     * @return	string
-     */
-    public function read($id) {
-	// obtain a read lock on the data, or subsequently wait for
-	// the lock to be released
-	$this->_lock($id);
+	/**
+	 * Read the session data.
+	 *
+	 * @access	public
+	 * @param	string	$id
+	 * @return	string
+	 */
+	public function read($id) {
+		// obtain a read lock on the data, or subsequently wait for
+		// the lock to be released
+		$this->_lock($id);
 
-        // exclude results that are inactive or expired
-        $result = $this->_mongo->findOne(
+		// exclude results that are inactive or expired
+		$result = $this->_mongo->findOne(
 			array(
 				'session_id'	=> $id,
 				'expiry'    	=> array('$gte' => time()),
@@ -231,41 +231,41 @@ class MongoSession {
 			)
 		);
 
-        if (isset($result['data'])) {
-            $this->_session = $result;
-            return $result['data'];
-        }
+		if (isset($result['data'])) {
+			$this->_session = $result;
+			return $result['data'];
+		}
 
-        return '';
-   }
+		return '';
+	}
 
-    /**
-     * Atomically write data to the session, ensuring we remove any
-     * read locks.
-     *
-     * @access  public
-     * @param   string  $id
-     * @param   mixed   $data
-     * @return	bool
-     */
-    public function write($id, $data) {
-        // create expires
-        $expiry = time() + $this->_config['lifetime'];
+	/**
+	 * Atomically write data to the session, ensuring we remove any
+	 * read locks.
+	 *
+	 * @access  public
+	 * @param   string  $id
+	 * @param   mixed   $data
+	 * @return	bool
+	 */
+	public function write($id, $data) {
+		// create expires
+		$expiry = time() + $this->_config['lifetime'];
 
-        // create new session data
-        $new_obj = array(
-            'data'		=> $data,
+		// create new session data
+		$new_obj = array(
+			'data'		=> $data,
 			'lock'		=> 0,
-            'active'		=> 1,
-            'expiry'		=> $expiry
-        );
+			'active'		=> 1,
+			'expiry'		=> $expiry
+		);
 
-        // check for existing session for merge
-        if (!empty($this->_session)) {
-            $obj = (array) $this->_session;
-            $new_obj = array_merge($obj, $new_obj);
-	    unset($new_obj['_id']); // cannot update mongoID
-        }
+		// check for existing session for merge
+		if (!empty($this->_session)) {
+			$obj = (array) $this->_session;
+			$new_obj = array_merge($obj, $new_obj);
+			unset($new_obj['_id']); // cannot update mongoID
+		}
 
 		// atomic update
 		$query = array('session_id' => $id);
@@ -285,45 +285,45 @@ class MongoSession {
 			return false;
 		}
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Destroys the session by removing the document with
-     * matching session_id.
-     *
-     * @access  public
-     * @param   string  $id
-     * @return  bool
-     */
-    public function destroy($id) {
-        $this->_mongo->remove(array('session_id' => $id), true);
-        return true;
-    }
+	/**
+	 * Destroys the session by removing the document with
+	 * matching session_id.
+	 *
+	 * @access  public
+	 * @param   string  $id
+	 * @return  bool
+	 */
+	public function destroy($id) {
+		$this->_mongo->remove(array('session_id' => $id), true);
+		return true;
+	}
 
-    /**
-     * Garbage collection. Remove all expired entries atomically.
-     *
-     * @access  public
-     * @return	bool
-     */
-    public function gc() {
-	// define the query
-	$query = array('expiry' => array('$lt' => time()));
+	/**
+	 * Garbage collection. Remove all expired entries atomically.
+	 *
+	 * @access  public
+	 * @return	bool
+	 */
+	public function gc() {
+		// define the query
+		$query = array('expiry' => array('$lt' => time()));
 
-	// specify the update vars
-	$update = array('$set' => array('active' => 0));
+		// specify the update vars
+		$update = array('$set' => array('active' => 0));
 
-	// update options
-	$options = array(
-		'multiple'	=> TRUE,
-		'safe'		=> TRUE,
-		'fsync'		=> TRUE
-	);
+		// update options
+		$options = array(
+			'multiple'	=> TRUE,
+			'safe'		=> TRUE,
+			'fsync'		=> TRUE
+		);
 
-	// update expired elements and set to inactive
-	$this->_mongo->update($query, $update, $options);
-	return true;
+		// update expired elements and set to inactive
+		$this->_mongo->update($query, $update, $options);
+		return true;
 	}
 
 	/**
@@ -347,35 +347,30 @@ class MongoSession {
 		$remaining = 30000000;
 		$timeout = 5000;
 
-        do {
+		do {
+			try {
+				$query = array('session_id' => $id, 'lock' => 0);
+				$update = array('$set' => array('lock' => 1));
+				$options = array('safe' => true, 'upsert' => true);
+				$result = $this->_mongo->update($query, $update, $options);
+				if ($result['ok'] == 1) {
+					return true;
+				}
+			} catch (MongoCursorException $e) {
+				if (substr($e->getMessage(), 0, 26) != 'E11000 duplicate key error') {
+				throw $e; // not a dup key?
+			}
+		}
 
-            try {
+		// force delay in microseconds
+		usleep($timeout);
+		$remaining -= $timeout;
 
-                $query = array('session_id' => $id, 'lock' => 0);
-                $update = array('$set' => array('lock' => 1));
-                $options = array('safe' => true, 'upsert' => true);
-                $result = $this->_mongo->update($query, $update, $options);
-                if ($result['ok'] == 1) {
-                    return true;
-                }
+		// backoff on timeout, save a tree. max wait 1 second
+		$timeout = ($timeout < 1000000) ? $timeout * 2 : 1000000;
 
-            } catch (MongoCursorException $e) {
-                if (substr($e->getMessage(), 0, 26) != 'E11000 duplicate key error') {
-                    throw $e; // not a dup key?
-                }
-            }
-
-			// force delay in microseconds
-            usleep($timeout);
-            $remaining -= $timeout;
-
-            // backoff on timeout, save a tree. max wait 1 second
-            $timeout = ($timeout < 1000000) ? $timeout * 2 : 1000000;
-
-        } while ($remaining > 0);
-
-        // aww shit.
-        throw new Exception('Could not obtain a session lock.');
-	}
-
+		} while ($remaining > 0);
+			// aww shit.
+			throw new Exception('Could not obtain a session lock.');
+		}
 }
