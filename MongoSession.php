72587c94c6954f9d1355ea4ecd102895102f657e
David Mitchel <git@mitzip.com>
2012-03-25 22:17:03 -0500

    First half of formatting changes

--- MongoSession.php
+++ MongoSession.php
@@ -9,10 +9,10 @@
  *
  * @author		Corey Ballou
  * @copyright	Corey Ballou (2010)
- * 
+ *
  */
 class MongoSession {
-	
+
     // default config with support for multiple servers
     // (helpful for sharding and replication setups)
     protected $_config = array(
@@ -25,23 +25,23 @@ class MongoSession {
         'database'      => 'session',   // name of MongoDB database
         'collection'    => 'session',   // name of MongoDB collection
 
-		// persistent related vars
-		'persistent' 	=> false, 			// persistent connection to DB?
+	// persistent related vars
+	'persistent' 	=> false, 		// persistent connection to DB?
         'persistentId' 	=> 'MongoSession', 	// name of persistent connection
-		
-		// whether we're supporting replicaSet
-		'replicaSet'		=> false,
 
-		// array of mongo db servers
+	// whether we're supporting replicaSet
+	'replicaSet'		=> false,
+
+	// array of mongo db servers
         'servers'   	=> array(
-            array(
-                'host'          => Mongo::DEFAULT_HOST,
-                'port'          => Mongo::DEFAULT_PORT,
-                'username'      => null,
-                'password'      => null
-            )
-        )
-    );
+		array(
+                	'host'          => Mongo::DEFAULT_HOST,
+                	'port'          => Mongo::DEFAULT_PORT,
+                	'username'      => null,
+                	'password'      => null
+            		)
+        	)
+	);
 
 	// stores the connection
 	protected $_connection;
@@ -58,8 +58,7 @@ class MongoSession {
      * @access  public
      * @param   array   $config
      */
-    public function __construct($config = array())
-	{
+    public function __construct($config = array()) {
         // initialize the database
         $this->_init(empty($config) ? $this->_config : $config);
 
@@ -89,7 +88,7 @@ class MongoSession {
 
         // disable client/proxy caching
         session_cache_limiter('nocache');
-        
+
         // set the cookie parameters
         session_set_cookie_params(
 			$this->_config['lifetime'],
@@ -99,7 +98,7 @@ class MongoSession {
 
         // name the session
         session_name('mongo_sess');
-    
+
         // start it up
         session_start();
 	}
@@ -111,20 +110,19 @@ class MongoSession {
      * @access  private
      * @param   array   $config
      */
-    private function _init($config)
-    {
+    private function _init($config) {
         // ensure they supplied a database
         if (empty($config['database'])) {
             throw new Exception('You must specify a MongoDB database to use for session storage.');
         }
-        
+
         if (empty($config['collection'])) {
             throw new Exception('You must specify a MongoDB collection to use for session storage.');
         }
-        
+
         // update config
         $this->_config = $config;
-        
+
         // generate server connection strings
         $connections = array();
         if (!empty($this->_config['servers'])) {
@@ -141,10 +139,10 @@ class MongoSession {
             // use default connection settings
             array_push($connections, Mongo::DEFAULT_HOST . ':' . Mongo::DEFAULT_PORT);
         }
-        
+
 		// add immediate connection
 		$opts = array('connect' => true);
-		
+
 		// support persistent connections
 		if ($this->_config['persistent'] && !empty($this->_config['persistentId'])) {
             $opts['persist'] = $this->_config['persistentId'];
@@ -154,28 +152,28 @@ class MongoSession {
 		if ($this->_config['replicaSet']) {
 			$opts['replicaSet'] = true;
 		}
-		
+
         // load mongo server connection
 		try {
 			$this->_connection = new Mongo('mongodb://' . implode(',', $connections), $opts);
 		} catch (Exception $e) {
 			throw new Exception('Can\'t connect to the MongoDB server.');
 		}
-        
+
         // load the db
         try {
             $mongo = $this->_connection->selectDB($this->_config['database']);
         } catch (InvalidArgumentException $e) {
             throw new Exception('The MongoDB database specified in the config does not exist.');
         }
-        
+
         // load collection
         try {
             $this->_mongo = $mongo->selectCollection($this->_config['collection']);
         } catch(Exception $e) {
             throw new Exception('The MongoDB collection specified in the config does not exist.');
         }
-        
+
         // proper indexing on the expiration
         $this->_mongo->ensureIndex(
 			array('expiry' => 1),
@@ -185,7 +183,7 @@ class MongoSession {
 				  'safe' => true
 			)
 		);
-		
+
 		// proper indexing of session id and lock
 		$this->_mongo->ensureIndex(
 			array('session_id' => 1, 'lock' => 1),
@@ -195,7 +193,7 @@ class MongoSession {
 				  'safe' => true
 			)
 		);
-    }
+	}
 
     /**
      * Open does absolutely nothing as we already have an open connection.
@@ -203,8 +201,7 @@ class MongoSession {
      * @access  public
      * @return	bool
      */
-    public function open($save_path, $session_name)
-    {
+    public function open($save_path, $session_name) {
         return true;
     }
 
@@ -215,8 +212,7 @@ class MongoSession {
      * @access  public
      * @return	bool
      */
-    public function close()
-    {
+    public function close() {
         return true;
     }
 
@@ -227,11 +223,10 @@ class MongoSession {
      * @param	string	$id
      * @return	string
      */
-    public function read($id)
-    {
-		// obtain a read lock on the data, or subsequently wait for
-		// the lock to be released
-		$this->_lock($id);
+    public function read($id) {
+	// obtain a read lock on the data, or subsequently wait for
+	// the lock to be released
+	$this->_lock($id);
 
         // exclude results that are inactive or expired
         $result = $this->_mongo->findOne(
@@ -259,8 +254,7 @@ class MongoSession {
      * @param   mixed   $data
      * @return	bool
      */
-    public function write($id, $data)
-    {
+    public function write($id, $data) {
         // create expires
         $expiry = time() + $this->_config['lifetime'];
 
@@ -271,7 +265,7 @@ class MongoSession {
             'active'		=> 1,
             'expiry'		=> $expiry
         );
-        
+
         // check for existing session for merge
         if (!empty($this->_session)) {
             $obj = (array) $this->_session;
@@ -280,14 +274,14 @@ class MongoSession {
 
 		// atomic update
 		$query = array('session_id' => $id);
-		
+
 		// update options
 		$options = array(
 			'upsert' 	=> TRUE,
 			'safe'		=> TRUE,
 			'fsync'		=> TRUE
 		);
-  
+
 		// perform the update or insert
 		try {
 			$result = $this->_mongo->update($query, array('$set' => $new_obj), $options);
@@ -295,7 +289,7 @@ class MongoSession {
 		} catch (Exception $e) {
 			return false;
 		}
-		
+
         return true;
     }
 
@@ -307,8 +301,7 @@ class MongoSession {
      * @param   string  $id
      * @return  bool
      */
-    public function destroy($id)
-    {
+    public function destroy($id) {
         $this->_mongo->remove(array('session_id' => $id), true);
         return true;
     }
@@ -319,38 +312,35 @@ class MongoSession {
      * @access  public
      * @return	bool
      */
-	public function gc()
-	{
-		// define the query
-		$query = array('expiry' => array('$lt' => time()));
-		
-		// specify the update vars
-		$update = array('$set' => array('active' => 0));
-		
-		// update options
-		$options = array(
-			'multiple'	=> TRUE,
-			'safe'		=> TRUE,
-			'fsync'		=> TRUE
-		);
-		
-		// update expired elements and set to inactive
-		$this->_mongo->update($query, $update, $options);
+    public function gc() {
+	// define the query
+	$query = array('expiry' => array('$lt' => time()));
 
-		return true;
-   	}
-	
+	// specify the update vars
+	$update = array('$set' => array('active' => 0));
+
+	// update options
+	$options = array(
+		'multiple'	=> TRUE,
+		'safe'		=> TRUE,
+		'fsync'		=> TRUE
+	);
+
+	// update expired elements and set to inactive
+	$this->_mongo->update($query, $update, $options);
+	return true;
+	}
+
 	/**
 	 * Solves issues with write() and close() throwing exceptions.
 	 *
 	 * @access	public
 	 * @return	void
 	 */
-	public function __destruct()
-	{
+	public function __destruct() {
 		session_write_close();
 	}
-	
+
 	/**
 	 * Create a global lock for the specified document.
 	 *
@@ -358,15 +348,14 @@ class MongoSession {
 	 * @access	private
 	 * @param	string	$id
 	 */
-	private function _lock($id)
-	{
+	private function _lock($id) {
 		$remaining = 30000000;
 		$timeout = 5000;
-		
+
         do {
-			
+
             try {
-				
+
                 $query = array('session_id' => $id, 'lock' => 0);
                 $update = array('$set' => array('lock' => 1));
                 $options = array('safe' => true, 'upsert' => true);
